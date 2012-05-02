54a24b76359232783dcc84991361c5362642d28d
thechaoz992 <TheChaoz992@gmail.com>
2012-05-02 00:43:20 +0000

    Part of version 2.1 uloaded manualy due to svn problems.

--- Trunk/Scripting Machine/Scripting Machine/Srch.Designer.vb
+++ Trunk/Scripting Machine/Scripting Machine/Srch.Designer.vb
@@ -39,15 +39,15 @@ Partial Class Srch
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Srch))
-        Me.Label1 = New System.Windows.Forms.Label
-        Me.TextBox1 = New System.Windows.Forms.TextBox
-        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(10, 15)
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 0
@@ -55,14 +55,14 @@ Partial Class Srch
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(60, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(62, 12)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(159, 20)
         Me.TextBox1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(225, 10)
+        Me.Button1.Location = New System.Drawing.Point(227, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(45, 23)
         Me.Button1.TabIndex = 2
